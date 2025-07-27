58a42a98c957554a84f9d9273fbb6316dcff2239
Anton Komarev <ell@cybercog.su>
2025-07-27 19:19:47 +0300

    Improve documentation

--- src/Locker/PostgresAdvisoryLockTypeEnum.php
+++ src/Locker/PostgresAdvisoryLockTypeEnum.php
@@ -7,8 +7,16 @@ namespace Cog\DbLocker\Locker;
 /**
  * PostgresAdvisoryLockTypeEnum defines the type of advisory lock acquisition.
  *
- * - NonBlocking: Attempt to acquire the lock without blocking (PG_TRY_ADVISORY_LOCK, PG_TRY_ADVISORY_XACT_LOCK).
- * - Blocking: Acquire the lock, blocking until it becomes available (PG_ADVISORY_LOCK, PG_ADVISORY_XACT_LOCK).
+ * - NonBlocking. Attempt to acquire the lock without blocking:
+ *      - PG_TRY_ADVISORY_LOCK
+ *      - PG_TRY_ADVISORY_LOCK_SHARE
+ *      - PG_TRY_ADVISORY_XACT_LOCK
+ *      - PG_TRY_ADVISORY_XACT_LOCK_SHARE
+ * - Blocking. Acquire the lock, blocking until it becomes available:
+ *      - PG_ADVISORY_LOCK
+ *      - PG_ADVISORY_LOCK_SHARE
+ *      - PG_ADVISORY_XACT_LOCK
+ *      - PG_ADVISORY_XACT_LOCK_SHARE
  */
 enum PostgresAdvisoryLockTypeEnum
 {
